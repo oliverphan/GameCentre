a255ba1227fe535db98ec71d186b6d572add6828
Meirbek <meirbek.zeinulla@mail.utoronto.ca>
2018-11-17 19:45:33 -0500

    Created Skeleton for Fragment Display

---
+++ Phase2/GameCentre/app/src/main/java/gamelauncher/MainActivity.java
@@ -0,0 +1,33 @@
+package gamelauncher;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+
+import fall2018.csc2017.slidingtiles.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main";
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting.");
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new SlidingTileActivity());
+//        adapter.addFragment(new ConnectFourActivity());
+//        adapter.addFragment(new MatchingGameActivity());
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ Phase2/GameCentre/app/src/main/java/gamelauncher/SectionsPageAdapter.java
@@ -0,0 +1,40 @@
+package gamelauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsPageAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+//    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    SectionsPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+
+}
