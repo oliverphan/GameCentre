2179aac93c441d1884ef976e89c5fdbafeff8c2c
Meirbek <meirbek.zeinulla@mail.utoronto.ca>
2018-11-23 16:54:54 -0500

    Added JavaDocs and annotations to BoardManager

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/connectFour/FourBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/connectFour/FourBoardManager.java
@@ -4,41 +4,83 @@ import java.io.Serializable;
 import java.util.Stack;
 
 public class FourBoardManager implements Serializable {
+    /**
+     * The Board being managed
+     */
     private FourBoard board;
+
+    /**
+     * The amount of moves the player has completed
+     */
     private int numMoves;
 //    Stack<Integer> previousMoves;
+    /**
+     * The difficulty of the current game.
+     */
     private int difficulty;
 
-    FourBoard getBoard(){
+    /**
+     * Return the current board being managed
+     *
+     * @return the board being managed
+     */
+    FourBoard getBoard() {
         return this.board;
     }
 
-    public FourBoardManager(int d){
+    /**
+     * Create a BoardManager with the selected difficulty
+     *
+     * @param d the difficulty selected
+     */
+    public FourBoardManager(int d) {
         difficulty = d;
 //        previousMoves = new Stack<>();
         board = new FourBoard();
     }
 
-    boolean isValidTap(int position){
+    /**
+     * Checks whether current tap is valid
+     *
+     * @param position the position tapped
+     * @return boolean whether the tap is valid or not
+     */
+    boolean isValidTap(int position) {
         int col = position % board.NUM_COLS;
         return board.openRow(col) > -1;
     }
 
-    void makeMove(int position){
+    /**
+     * Completes move that is selected
+     *
+     * @param position the position tapped
+     */
+    void makeMove(int position) {
         int col = position % board.NUM_COLS;
-        if (board.openRow(col) > -1){
+        if (board.openRow(col) > -1) {
             numMoves++;
 //            previousMoves.push(col);
             board.makeMove(col, board.curPlayer);
         }
     }
 
-    public int getDifficulty() {
+    /**
+     * Return the difficulty of the board manager
+     *
+     * @return the difficulty
+     */
+    int getDifficulty() {
         return difficulty;
     }
 
-    public int generateScore(){
-        return 100*difficulty*numMoves - (board.isWinner(1) ? 0 : 10*difficulty);
+    /**
+     * Generate the score for the current game
+     * a more difficult game yields a higher score, losing subtracts a higher amount from score
+     *
+     * @return the score generated
+     */
+    int generateScore() {
+        return 100 * difficulty * numMoves - (board.isWinner(1) ? 0 : 10 * difficulty);
     }
 //    void undoMove(int numToUndo) {
 //        if (!gameFinished()) {
@@ -60,7 +102,12 @@ public class FourBoardManager implements Serializable {
 //        }
 //    }
 
-    boolean gameFinished(){
+    /**
+     * Returns if the game is finished whether the board is full or there is a winner
+     *
+     * @return true if the game is finished
+     */
+    boolean gameFinished() {
         return board.isBoardFull() || board.isWinner(1) || board.isWinner(2);
     }
 }
