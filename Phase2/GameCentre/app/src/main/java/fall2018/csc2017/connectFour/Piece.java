4db74359b6b2202a5c26b646ea5ba9b11a1fce27
Meirbek <meirbek.zeinulla@mail.utoronto.ca>
2018-11-23 16:49:56 -0500

    Annotated and added JavaDocs to Piece

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/connectFour/Piece.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/connectFour/Piece.java
@@ -4,42 +4,57 @@ import java.io.Serializable;
 
 import fall2018.csc2017.slidingtiles.R;
 
+/**
+ * The Piece in a Connect Four game.
+ */
 public class Piece implements Serializable {
+    /**
+     * Player that the piece belongs to.
+     */
     private int player;
+
+    /**
+     * The background id of the Piece
+     */
     private int background;
 
-    Piece(){
+    /**
+     * A piece with the empty_piece background and no player.
+     */
+    Piece() {
         this.player = 0;
         this.background = R.drawable.empty_piece;
     }
 
-    int getPlayer(){
+    /**
+     * Getter to return the player that this Piece belongs to.
+     *
+     * @return player that owns this piece (1: User, 2: AI, 0: empty)
+     */
+    int getPlayer() {
         return this.player;
     }
 
-    int getBackground(){
+    /**
+     * Return the background id of this piece
+     *
+     * @return the background id
+     */
+    int getBackground() {
         return this.background;
     }
 
-    void setPlayer(int player){
+    /**
+     * Set a new player that owns this Piece
+     *
+     * @param player int to designate the player that owns this Piece
+     */
+    void setPlayer(int player) {
         this.player = player;
-        if (player == 0){
+        if (player == 0) {
             this.background = R.drawable.empty_piece;
-        }else{
+        } else {
             this.background = (player == 1) ? R.drawable.piece_1 : R.drawable.piece_2;
         }
-    }
-
-    @Override
-    public boolean equals(Object o){
-        if (o == this){
-            return true;
-        }
-        if (!(o instanceof Piece)){
-            return false;
-        }
-        Piece p = (Piece) o;
-
-        return p.player == this.player;
     }
 }
