13d56d7d2c2c8005725cb56cb8eb6fd4421f9349
Meirbek <meirbek.zeinulla@mail.utoronto.ca>
2018-11-24 23:40:05 -0500

    LeaderBoard now opens to game that called it

--- Phase2/GameCentre/app/src/main/java/gamelauncher/ConnectFourActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamelauncher/ConnectFourActivity.java
@@ -23,6 +23,7 @@ import fall2018.csc2017.SaveAndLoad;
 import fall2018.csc2017.connectFour.FourBoardManager;
 import fall2018.csc2017.connectFour.FourGameActivity;
 import fall2018.csc2017.R;
+import scoring.LeaderBoardActivity;
 import users.User;
 
 public class ConnectFourActivity extends Fragment implements SaveAndLoad {
@@ -151,7 +152,9 @@ public class ConnectFourActivity extends Fragment implements SaveAndLoad {
         scoreBoardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createToast("Launched LeaderBoard");
+                Intent tmp = new Intent(getActivity(), LeaderBoardActivity.class);
+                tmp.putExtra("frgToLoad", 1);
+                startActivity(tmp);
             }
         });
 
