ac6da1359ed981375c709b5ab59e15e18588c000
Oliver Phan <oliver.phan@mail.utoronto.ca>
2018-11-24 22:05:48 -0500

    Refactored calls to app resources

--- Phase2/GameCentre/app/src/main/java/gamelauncher/SlidingTileActivity.java
+++ Phase2/GameCentre/app/src/main/java/gamelauncher/SlidingTileActivity.java
@@ -1,6 +1,6 @@
 package gamelauncher;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
