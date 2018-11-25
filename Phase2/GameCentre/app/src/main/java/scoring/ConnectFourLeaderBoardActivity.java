72642b914e6de170417c9c30e6ccdee83d13282d
jackhuang <jackjs.huang@mail.utoronto.ca>
2018-11-24 22:18:37 -0500

    Edited constraints for signin_xml textview

---
+++ Phase2/GameCentre/app/src/main/java/scoring/ConnectFourLeaderBoardActivity.java
@@ -0,0 +1,22 @@
+package scoring;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fall2018.csc2017.R;
+
+public class ConnectFourLeaderBoardActivity extends Fragment {
+
+    //    Basically onCreate
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.connectfour_leaderboard_activity, container, false);
+        Bundle args = getArguments();
+
+        return view;
+    }
+
+}
---
+++ Phase2/GameCentre/app/src/main/java/scoring/MemoryLeaderBoardActivity.java
@@ -0,0 +1,23 @@
+package scoring;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import fall2018.csc2017.R;
+
+public class MemoryLeaderBoardActivity extends Fragment {
+    //    Basically onCreate
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.memory_leaderboard_activity, container, false);
+        Bundle args = getArguments();
+
+        return view;
+        }
+
+}
